238f2952acfc2a495ace4dea25ab9c1619ea7e10
Alexey <leha_s78@mail.ru>
2023-06-12 17:48:34 +0300

    add Options App class (@CommandLine.Parameters)

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,9 +1,18 @@
 package hexlet.code;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+
+import java.io.File;
+
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.")
 public class App  implements Runnable {
+    @CommandLine.Parameters(index = "0", description = "path to first file")
+    private File filepath1;
+    @CommandLine.Parameters(index = "1", description = "path to second file")
+    private File filepath2;
+    @CommandLine.Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
+    private String algorithm;
     public static void main(String[] args) {
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
