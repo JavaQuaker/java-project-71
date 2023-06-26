631c95fb88c10325e25685f2895c14d265e66058
JavaQuaker <83884501+JavaQuaker@users.noreply.github.com>
2023-06-26 09:33:22 +0300

    Update App.java
    update App (@Override call)

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,6 +1,7 @@
 package hexlet.code;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import java.util.concurrent.Callable;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -9,22 +10,32 @@ import java.util.Map;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.")
-public class App  implements Runnable {
+public class App  implements Callable<Integer> {
     @CommandLine.Parameters(index = "0", description = "path to first file")
     private File filepath1;
     @CommandLine.Parameters(index = "1", description = "path to second file")
     private File filepath2;
     @CommandLine.Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
     private String algorithm;
+    private static final int SUCCESS_EXIT_CODE = 0;
+    private static final int ERROR_EXIT_CODE = 1;
+
     public static void main(String[] args) {
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
     }
-    @Override
-    public void run() {
 
+    @Override
+    public Integer call() {
+
+        try {
+            String formattedDiff = Differ.generate(filepath1, filepath2);
+            System.out.println(formattedDiff);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            return ERROR_EXIT_CODE;
+        }
+
+        return SUCCESS_EXIT_CODE;
     }
-    static File resultFile1 = new File("app/src/test/resources/Test_1.txt");
-    static File resultFile2 = new File("app/src/test/resources/Test_2.txt");
-    static List<Map<String, Object>> listMap = new ArrayList<>();
 }
