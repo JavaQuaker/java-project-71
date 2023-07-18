81d180ded1a3f1f5ce9d08b28f4f4348086cf1f7
Alexey <leha_s78@mail.ru>
2023-07-18 22:33:57 +0300

    ignore, add switch(formatter), IOException(formatter)

--- app/src/main/java/hexlet/code/Formatter.java
+++ app/src/main/java/hexlet/code/Formatter.java
@@ -1,22 +1,29 @@
 package hexlet.code;
-import com.fasterxml.jackson.core.JsonProcessingException;
+
 import hexlet.code.Formatters.FormatJson;
 import hexlet.code.Formatters.FormatPlain;
 import hexlet.code.Formatters.FormatStylish;
+
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
 public class Formatter {
     public static String formatting(List<Map<String, Object>> treeDiff, String formatName)
-            throws JsonProcessingException {
-        if (formatName.equals("stylish")) {
-            return FormatStylish.formattingStylish(treeDiff);
-        } else if (formatName.equals("plain")) {
-            return FormatPlain.formatPlain(treeDiff);
-        } else if (formatName.equals("json")) {
-            return FormatJson.formatJson(treeDiff);
+            throws IOException {
+        switch (formatName) {
+            case "stylish" -> {
+                return FormatStylish.formattingStylish(treeDiff);
+            }
+            case "plain" -> {
+                return FormatPlain.formatPlain(treeDiff);
+            }
+            case "json" -> {
+                return FormatJson.formatJson(treeDiff);
+            }
+            default -> {
+            }
         }
-        throw new RuntimeException("");
+        throw new IOException("");
     }
 }
-
